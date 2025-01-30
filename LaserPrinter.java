78942cbb7ef75aeef7b3163da2716f0177c15a90
P-Thanmaya <THANMAYA.PALADUGULA@CAPGEMINI.COM>
2025-01-30 11:22:02 +0530

    Revert "Sample1"

---
+++ LaserPrinter.java
@@ -0,0 +1,20 @@
+package Assignment;
+
+public class LaserPrinter implements ScanerCopier,Painter {
+	public String scanCopy() {
+		return "scanning copy nabled";
+		
+	}
+	
+    @Override
+	public String paint() {
+		// TODO Auto-generated method stub
+		return "laser printer started";
+		
+	}
+
+	
+
+
+
+}
